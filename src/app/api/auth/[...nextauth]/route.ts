48b2a39c87e195aa7e0d8232aaecb049eb9b2087
VinTechG2628 <vintech.g2628@gmail.com>
2024-10-12 10:09:28 +0530

    file updated

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,15 +1,26 @@
-// src/app/api/auth/[...nextauth]/route.tsx
-
 import NextAuth from 'next-auth';
-import GithubProvider from "next-auth/providers/github";
+import GithubProvider from 'next-auth/providers/github';
+import type { NextAuthOptions } from 'next-auth';
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { toast } from "react-toastify";
 
-const handler = NextAuth({
+const options: NextAuthOptions = {
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_ID as string,
       clientSecret: process.env.GITHUB_SECRET as string,
     }),
   ],
-});
+};
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  try {
+    await NextAuth(req, res, options);
+  } catch (error) {
+    toast.error("NextAuth error:" + error, { autoClose: 2000 });
+    console.error("NextAuth error:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
 
 export { handler as GET, handler as POST };
